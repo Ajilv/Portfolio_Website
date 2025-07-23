bdad50ecfff7c9cd0562621c7be4ab8723e8713f
Ajilv <51069619+Ajilv@users.noreply.github.com>
2025-07-23 12:50:01 +0530

    Added StaticFiles

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2mcjo@9#@(^nwo6avr3hdezzm@kpm8g&+f0awg^+f!okh0edor'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Ajilvsajith.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 
